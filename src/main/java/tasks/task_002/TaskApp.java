ed62df17cee73f98f6a96ecaa195768474299dd7
Yan Bondarenko <yan.bondarenko@gmail.com>
2018-08-18 22:35:20 +0300

    added 'getDirectAverageOfDigits' method to 'MathActions' class

--- src/main/java/tasks/task_002/TaskApp.java
+++ src/main/java/tasks/task_002/TaskApp.java
@@ -9,8 +9,9 @@ public class TaskApp {
     public static void main(String[] args) {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        System.out.println(MathActions.getDifferenceBetweenDoubleFigureInfo(reader));
-        System.out.println(MathActions.getTheLargestOfTheThreeNumbers(reader));
-        MathActions.getSmallestAndBiggestOfInputtedNumbers(reader);
+//        System.out.println(MathActions.getDifferenceBetweenDoubleFigureInfo(reader));
+//        System.out.println(MathActions.getTheLargestOfTheThreeNumbers(reader));
+//        MathActions.getSmallestAndBiggestOfInputtedNumbers(reader);
+        System.out.println(MathActions.getDirectAverageOfDigits(reader));
     }
 }
--- src/main/java/tasks/task_002/math/MathActions.java
+++ src/main/java/tasks/task_002/math/MathActions.java
@@ -123,4 +123,33 @@ public class MathActions {
         }
         System.out.println("\n- - - - -THE END");
     }
+
+    public static String getDirectAverageOfDigits(final BufferedReader reader) {
+        int number = 0;
+        try {
+            log.debug("Введите 7-ми значное число: ");
+            number = Integer.parseInt(reader.readLine());
+        } catch (IOException ioe) {
+            System.out.println("EXCEPTION - " + ioe);
+            ioe.printStackTrace();
+        }
+
+        int res1 = number / 1000000; // 1
+        int res2 = number / 100000; // 12
+        res2 = res2 % 10; // 2
+        int res3 = number / 10000; // 123
+        res3 = res3 % 10; // 3
+        int res4 = number / 1000; // 1234
+        res4 = res4 % 10; // 4
+        int res5 = number / 100; // 12345
+        res5 = res5 % 10; // 5
+        int res6 = number / 10; // 123456
+        res6 = res6 % 10; // 6
+        int res7 = number / 1; // 1234567
+        res7 = res7 % 10; // 7
+
+        int arithmeticMean = (res1 + res2 + res3 + res4 + res5 + res6 + res7) / 7;
+        String result = "Среднее арифметическое: " + arithmeticMean;
+        return result;
+    }
 }
