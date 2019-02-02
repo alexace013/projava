70fcbcea9c4e85da9a615ad6c72a0e62a2a98ae4
alexace013 <alexace013@gmail.com>
2019-02-02 23:46:40 +0200

    refactoring MathActionsTask1Test and MathActionsTask2Test. created MathActionsConstants class.

---
+++ src/test/java/tasks/course/MathActionsConstants.java
@@ -0,0 +1,13 @@
+package tasks.course;
+
+public class MathActionsConstants {
+
+    protected static final int A = 1;
+    protected static final int B = 2;
+    protected static final int C = 3;
+
+    protected int value = 0;
+    protected String msg = null;
+    protected static final String INPUT_MSG = "Input: %d";
+
+}
